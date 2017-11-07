b51c37ec42e132b9ea0a275debe201e032532fa3
Bram Borggreve <borggreve@gmail.com>
2017-11-08 00:25:02 +0700

    feat(web): add support for service worker

--- apps/web/src/app/app.module.ts
+++ apps/web/src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { NxModule } from '@nrwl/nx';
+
+import { UiModule } from '../../../../libs/ui';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserModule } from '@angular/platform-browser';
-import { NxModule } from '@nrwl/nx';
-import { UiModule } from '../../../../libs/ui';
+
+import { environment } from '../environments/environment'
 
 @NgModule({
   imports: [
     AppRoutingModule,
     BrowserModule.withServerTransition({ appId: 'angular-nx-ssr' }),
+    environment.production ? ServiceWorkerModule.register('/ngsw-worker.js') : [],
     NxModule.forRoot(),
     UiModule
   ],
